f03de16be91ea4f32842f5a8cf0162cf5276815b
rahulg1310 <rahulganesan3@gmail.com>
2026-05-28 01:00:18 +0530

    basic navigation

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Gamepad2 } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className='min-h-screen w-full bg-[#080c10] flex justify-center items-center px-4'>
       <div className='bg-[#111820] flex flex-col gap-5 py-8 px-8     rounded-2xl border border-white/15 items-center w-full max-w-110'>
@@ -14,18 +18,22 @@ const SignIn = () => {
             <h2 className='body text-xs text-[#8a9bb0]'>Sign in to your account to continue</h2>
         </div>
         <div className='flex flex-col w-full items-start'>
-            <form action="" className='w-full'>
+            <form 
+            onSubmit={(e)=>{
+                e.preventDefault();
+                navigate('/dashboard');
+            }}
+            action="" className='w-full'>
                 <span className='body text-[13px] text-[#8a9bb0]'>Email</span>
                 <input type="text" name="" id="" placeholder='user@example.com' className='body text-[13px] px-2 py-2 rounded-[10px] mt-1 mb-5 w-full bg-[#080c10] outline-none border border-gray-600/40 focus:border-cyan-400 transition-all duration-200' />
                 <span className='body text-[13px] text-[#8a9bb0]'>Password</span>
-                <input type="password" name="" id="" placeholder='Enter Password' className='body text-[13px] px-2 py-2 rounded-[10px] mt-1 w-full bg-[#080c10] outline-none border border-gray-600/40 focus:border-cyan-400 transition-all duration-200' />
+                <input type="password" name="" id="" placeholder='Enter Password' className='body text-[13px] px-2 py-2 rounded-[10px] mt-1 w-full bg-[#080c10] outline-none border border-gray-600/40 focus:border-cyan-400 transition-all duration-200 mb-5' />
+                <div className='w-full flex justify-end'>
+                    <button className='body text-[13px] text-[#38d60c] transition-all duration-200 mb-5'>Forgot Password?</button>
+                </div>
+                <button type='submit' className='font-bold tracking-wider text-black w-full bg-linear-to-r from-green-400 to-cyan-400 rounded-[7px] py-1.5 hover:-translate-y-0.5 hover:opacity-80 transition-all duration-200'
+                >Sign In</button>
             </form>
-        </div>
-        <div className='w-full flex justify-end'>
-            <button className='body text-[13px] text-[#38d60c] transition-all duration-200'>Forgot Password?</button>
-        </div>
-        <div className='w-full'>
-            <button type='submit' className='font-bold tracking-wider text-black w-full bg-linear-to-r from-green-400 to-cyan-400 rounded-[7px] py-1.5 hover:-translate-y-0.5 hover:opacity-80 transition-all duration-200'>Sign In</button>
         </div>
         <div className='w-full flex justify-center'>
             <span className='body text-xs text-[#4a5568]'>or continue with</span>
@@ -37,7 +45,11 @@ const SignIn = () => {
         </div>
         <div className='w-full flex justify-center' >
             <h1 className='body text-[13px] text-[#8a9bb0] mr-1'>Don't have an account?</h1>
-            <button className='body text-[13px] text-[#38d60c] transition-all duration-200'>Create One</button>
+            <button 
+            onClick={()=>{
+                navigate("/signup");
+            }}
+            className='body text-[13px] text-[#38d60c] transition-all duration-200'>Create One</button>
         </div>  
       </div>
     </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Gamepad2 } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const navigate = useNavigate();  
   return (
     <div className='min-h-screen w-full bg-[#080c10] flex justify-center items-center px-4'>
       <div className='bg-[#111820] flex flex-col gap-5 py-8 px-8     rounded-2xl border border-white/15 items-center w-full max-w-110'>
@@ -14,7 +16,12 @@ const SignUp = () => {
             <h2 className='body text-xs text-[#8a9bb0]'>Start tracking your gaming journey</h2>
         </div>
         <div className='flex flex-col w-full items-start'>
-            <form action="" className='w-full'>
+            <form 
+            onSubmit={(e)=>{
+                e.preventDefault();
+                navigate('/dashboard');
+            }}
+            action="" className='w-full'>
                 <span className='body text-[13px] text-[#8a9bb0]'>Username</span>
                 <input type="text" name="" id="" placeholder='TheRadBrad' className='body text-[13px] px-2 py-2 rounded-[10px] mt-1 mb-5 w-full bg-[#080c10] outline-none border border-gray-600/40 focus:border-cyan-400 transition-all duration-200' />
                 <span className='body text-[13px] text-[#8a9bb0]'>Email</span>
@@ -30,7 +37,11 @@ const SignUp = () => {
         
         <div className='w-full flex justify-center' >
             <h1 className='body text-[13px] text-[#8a9bb0] mr-1'>Already have an account?</h1>
-            <button className='body text-[13px] text-[#38d60c] transition-all duration-200 transition-all duration-200 hover:text-cyan-400'>Sign In</button>
+            <button 
+            onClick={()=>{
+                navigate("/");
+            }}
+            className='body text-[13px] text-[#38d60c] transition-all duration-200 transition-all duration-200 hover:text-cyan-400'>Sign In</button>
         </div>  
       </div>
     </div>
