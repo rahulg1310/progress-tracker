af07740467ef18e488c656c11e8e3383d213fe89
rahulg1310 <rahulganesan3@gmail.com>
2026-06-10 22:44:27 +0530

    profile page started

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import ProfileContent from '../components/ProfileContent'
 
 const Profile = () => {
   return (
     <div className='flex min-h-screen w-full bg-[#080c10]'>
         <Sidebar />
         <div className='flex-1 overflow-y-auto'>
-            
+            <ProfileContent />
         </div>
     </div>
   )
